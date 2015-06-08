a76321fc0bcad2a0cf6947dc8023223149aa9abf
JonDeen <jonas@ogaard.no>
2015-06-08 18:32:58 +0200

    back to modulus, float

--- libvips/colour/LabQ2sRGB.c
+++ libvips/colour/LabQ2sRGB.c
@@ -598,9 +598,7 @@ vips_col_sRGB2HSV( int range, float *lut,
 	if (delta == 0) {
 		*H = 0;
 	} else if (cma == c1) {
-		*H = ((c2 - c3) / delta);
-		while (H >= 6.0) *H = H-6;
-		*H = H*DEG2UCHAR;
+		*H = (((c2 - c3) / delta) % (float)6.0) * DEG2UCHAR;
 	} else if (cma == c2) {
 		*H = (((c3 - c1) / delta) + 2) * DEG2UCHAR;
 	} else if (cma == c3) {
