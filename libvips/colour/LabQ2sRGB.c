e59da7fa9fba019ceb1ca8c3e1996d5629479950
JonDeen <jonas@ogaard.no>
2015-06-09 09:16:37 +0200

    casting

--- libvips/colour/LabQ2sRGB.c
+++ libvips/colour/LabQ2sRGB.c
@@ -598,7 +598,7 @@ vips_col_sRGB2HSV( int range, int r, int g, int b, float *H, float *S, float *V 
 		*H = 0;
 	} else if (cma == c1) {
 		*H = ((c2 - c3) / delta)  * normalization;
-		if (c2 < c3) *H = H + (float) 256.0;
+		if (c2 < c3) *H = &H + (float) 256.0;
 	} else if (cma == c2) {
 		*H = (((c3 - c1) / delta) + 2) * normalization;
 	} else if (cma == c3) {
